14b0e22a7a43c0be9434c7ba436310c9ede37135
Manuel <joseguns2@gmail.com>
2025-03-13 21:14:26 -0700

    Modifique DaisyUI

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import daisyui from 'daisyui';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -14,8 +16,8 @@ export default {
       },
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
   daisyui: {
-    themes: ["light", "dark", "cupcake"], // Puedes personalizar los temas que quieras incluir
+    themes: ["light", "dark", "cupcake"],
   },
 }
